dfb028d4dcc5ab728537265ee2e277e55272b098
most1011 <st.moser@gmx.de>
2018-01-04 19:28:16 +0100

    Grundgerüst Kapazitätsplanung

---
+++ src/main/webapp/app/periodplanning/in-house_production/in-house_production.component.ts
@@ -0,0 +1,48 @@
+
+
+import { Component, OnInit } from '@angular/core'; 
+ //Referenz zu einem bereits offenem Fenster - um Instanzen zu übergeben, die in diesem Fenster befüllt werden 
+//import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'; 
+ 
+ 
+//import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; 
+ 
+//import { ActivatedRoute, Router } from '@angular/router'; 
+ //Wird benötigt, wenn Buttons angeklickt werden 
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster'; 
+ 
+ //Um Berechtigungen Prüfen zu können 
+//import { ITEMS_PER_PAGE, Principal, User, UserService, ResponseWrapper } from '../../shared'; 
+ 
+ //import { KaufteileService } from './kaufteile.service'; 
+ //import { userService } from './kaufteile.service'; 
+ 
+@Component({ 
+    selector: 'jhi-in-house_production', 
+     //Auswahlmenue in der Navigationsleiste 
+    templateUrl: './in-house_production.component.html', 
+     //Fenstergröße --> hier: Standard 
+     //styles: [] 
+}) 
+export class InHouseProductionComponent implements OnInit { 
+    //Variablen, die ich benötige hier definieren:  
+    
+     
+    constructor() {} 
+        private eventManager: JhiEventManager; 
+ 
+    ngOnInit() { 
+           
+    } 
+ 
+     
+    //hier Funktionen einbauen:  
+
+} 
+     
+         
+         
+         
+         
+         
+   
--- src/main/webapp/app/periodplanning/index.ts
+++ src/main/webapp/app/periodplanning/index.ts
@@ -3,3 +3,7 @@ export * from './periodplanning.module';
 export * from './purchased_part/purchased_part.component'; 
 //export * from './purchased part/purchased_part.service'; 
 export * from './purchased_part/purchased_part.route'; 
+export * from './in-house_production/in-house_production.component'; 
+export * from './in-house_production/in-house_production.route'; 
+export * from './capacity_planning/capacity_planning.component'; 
+export * from './capacity_planning/capacity_planning.route';
